fbce354a578f1ee1b7e146ae2691abecc89108e1
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-09-05 16:29:12 +0200

    MISC: Deny access from logs folder.

--- lib/capistrano-exts/receipts/contao.rb
+++ lib/capistrano-exts/receipts/contao.rb
@@ -32,6 +32,11 @@ Capistrano::Configuration.instance(:must_exist).load do
         #{try_sudo} mkdir -p #{shared_path}/contents/audio &&
         #{try_sudo} mkdir -p #{shared_path}/contents/pdf
       CMD
+
+      deny_htaccess = "order deny,allow"
+      deny_htaccess << "deny from all"
+
+      put deny_htaccess, "#{shared_path}/logs/.htaccess"
     end
 
     desc "[internal] Setup contao's localconfig"
