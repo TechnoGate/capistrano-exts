cd32fbb3b57c90890a79f61b54b4dbf9e634ee87
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-09-07 11:40:24 +0200

    MISC: Add a task to import the contents folder.

--- lib/capistrano-exts/receipts/contents.rb
+++ lib/capistrano-exts/receipts/contents.rb
@@ -95,9 +95,57 @@ Capistrano::Configuration.instance(:must_exist).load do
       puts "Contents has been downloaded to #{export_filename}"
       exit 0
     end
+
+    desc "Import the contents folder"
+    task :import, :roles => :app, :except => { :no_release => true } do
+      # Find out at which index the file is located ?
+      argv_file_index = ARGV.index("contents:import") + 1
+
+      unless ARGV.size >= (argv_file_index + 1) and File.exists?(ARGV[argv_file_index])
+        puts "ERROR: please run 'cap import <gzipped tar>'"
+        exit 1
+      else
+        # The contents file name
+        import_filename_argv = ARGV[argv_file_index]
+        # Read the dump
+        contents_dump = File.read(import_filename_argv)
+        # Generate a random file
+        random_file = random_tmp_file contents_dump
+        # Add a rollback hook
+        on_rollback { run "rm -f #{random_file}" }
+
+        # Ask for a confirmation
+        response = ask("I am going to add/replace all the files in the contents folder of #{fetch :application} with the contents of #{import_filename_argv}, are you sure you would like to continue (Yes, [No], Abort)", default:'N')
+        if response =~ /(no?)|(a(bort)?|\n)/i
+          abort "Canceled by the user."
+        end
+
+        # Transfer the SQL file to the server
+        # TODO: Try upload(filename, remote_file_name) function instead
+        put contents_dump, random_file
+
+        run <<-CMD
+          cd #{shared_path}/shared_contents &&
+          tar xzf #{random_file}
+        CMD
+
+        # Remove the uploaded file
+        run <<-CMD
+          rm -f '#{random_file}'
+        CMD
+
+        # Fix permissions
+        find_and_execute_task("deploy:fix_permissions")
+
+        # Exit because capistrano will rollback, the next argument is a file name and not a task
+        # TODO: Find a better solution!
+        exit 0
+      end
+    end
   end
 
   after "deploy:setup", "contents:setup"
   after "deploy:finalize_update", "contents:fix_links"
   before "contents:export", "contents:backup"
+  before "contents:import", "contents:backup"
 end
