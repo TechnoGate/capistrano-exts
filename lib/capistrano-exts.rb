TechnoGate/capistrano-exts

commit 6fba29db7e15f895265a07946b1b9ca0dbe14b82

MISC: Use capistrano_colors to make it easy to spot config contents.

lib/capistrano-exts.rb
@@ -7,8 +7,23 @@ $: << ROOT_PATH if File.directory?(ROOT_PATH) and not $:.include?(ROOT_PATH)
 # Require our core extensions
 require 'capistrano-exts/core_ext'
 
+# require Capistrano colors
+require 'capistrano/configuration'
+require 'capistrano/logger'
+require 'capistrano_colors/configuration'
+require 'capistrano_colors/logger'
+
+# Printed credentials
+Capistrano::Logger.add_color_matcher({ :match => /adapter:|hostname:|username:|password:/, :color => :red, :level => Capistrano::Logger::TRACE, :prio => -20, :attribute => :blink })
+
+# Warnings
+Capistrano::Logger.add_color_matcher({ :match => /WARNING:/, :color => :yellow, :level => Capistrano::Logger::INFO, :prio => -20 })
+
+# Errors
+Capistrano::Logger.add_color_matcher({ :match => /ERROR:/, :color => :red, :level => Capistrano::Logger::IMPORTANT, :prio => -20 })
+
 # Require requested receipts
-require 'capistrano-exts/receipts' if defined?(Capistrano::Configuration)
+require 'capistrano-exts/receipts'
 
 # Require all servers
 Dir["#{ROOT_PATH}/capistrano-exts/servers/*.rb"].each { |f| require f }

capistrano-exts.gemspec
@@ -27,6 +27,7 @@ EOD
 
   # Run-time dependencies
   s.add_dependency('capistrano', '~>2.8.0')
+  s.add_dependency('capistrano_colors', '~>0.5.5')
   s.add_dependency('i18n', '~>0.6.0')
   s.add_dependency('activesupport', '~>3.1.0')
 

lib/capistrano-exts/receipts/contao.rb
@@ -48,7 +48,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
         # localconfig
         if mysql_credentials.blank?
-          puts "WARNING: The mysql credential file can't be found, localconfig has just been copied from the sample file"
+          logger.info "WARNING: The mysql credential file can't be found, localconfig has just been copied from the sample file"
         end
 
         # Add MySQL credentials
@@ -61,7 +61,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
         put localconfig, localconfig_php_config_path
       else
-        puts "WARNING: The file '#{localconfig_php_config_path}' already exists, not overwriting."
+        logger.info "WARNING: The file '#{localconfig_php_config_path}' already exists, not overwriting."
       end
     end
 

lib/capistrano-exts/receipts/contents.rb
@@ -92,7 +92,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       # Tranfer the contents to the local system
       get latest_contents_backup, export_filename
 
-      puts "Contents has been downloaded to #{export_filename}"
+      logger.info "Contents has been downloaded to #{export_filename}"
       exit 0
     end
 
@@ -102,7 +102,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       argv_file_index = ARGV.index("contents:import") + 1
 
       unless ARGV.size >= (argv_file_index + 1) and File.exists?(ARGV[argv_file_index])
-        puts "ERROR: please run 'cap import <gzipped tar>'"
+        logger.important "ERROR: please run 'cap import <gzipped tar>'"
         exit 1
       else
         # The contents file name

lib/capistrano-exts/receipts/deploy.rb
@@ -13,8 +13,8 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Check if the remote is ready, should we run cap deploy:setup?"
     task :check_if_remote_ready, :roles => :web do
       unless remote_file_exists?("#{shared_path}")
-        puts "ERROR: The project is not ready for deployment."
-        puts "please run `cap deploy:setup"
+        logger.important "ERROR: The project is not ready for deployment."
+        logger.important "please run `cap deploy:setup"
         exit
       end
     end
@@ -67,12 +67,12 @@ Capistrano::Configuration.instance(:must_exist).load do
               #{try_sudo} ln -nsf #{fetch :public_path} #{deploy_to}/#{folder}
             CMD
           rescue Capistrano::CommandError
-            puts "WARNING: I couldn't replace the old htdocs please do so manually"
+            logger.info "WARNING: I couldn't replace the old htdocs please do so manually"
           end
         end
       end
 
-      puts "The public folders has been moved to the old folder"
+      logger.info "The public folders has been moved to the old folder"
     end
   end
 

lib/capistrano-exts/receipts/functions.rb
@@ -38,7 +38,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           run <<-CMD
             #{try_sudo} touch #{path}/#{file_name}
           CMD
-          puts "WARNING: You should edit '#{path}/#{file_name}' or re-create it as a folder if that's your intention."
+          logger.info "WARNING: You should edit '#{path}/#{file_name}' or re-create it as a folder if that's your intention."
         end
       end
 

lib/capistrano-exts/receipts/git.rb
@@ -12,8 +12,8 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Check if the branch is ready"
     task :check_if_branch_is_ready, :roles => :app, :except => { :no_release => true } do
       unless `git rev-parse #{branch}` == `git rev-parse origin/#{branch}`
-        puts "ERROR: #{branch} is not the same as origin/#{branch}"
-        puts "Run `git push` to sync changes."
+        logger.important "ERROR: #{branch} is not the same as origin/#{branch}"
+        logger.important "Run `git push` to sync changes."
         exit
       end
     end

lib/capistrano-exts/receipts/multistage.rb
@@ -70,7 +70,7 @@ Capistrano::Configuration.instance.load do
             f.write File.read(File.expand_path(File.join File.dirname(__FILE__), '..', 'templates', 'multistage.rb'))
           end
 
-          puts "#{name} configurations has been written to #{file}, please open and edit it."
+          logger.info "#{name} configurations has been written to #{file}, please open and edit it."
         end
       end
     end

commit 1f966a4be6b8a5f9a92863b0e2b90b2e6cb01957

MISC: Revert part of 6fba29d which broke testing.

lib/capistrano-exts.rb
@@ -8,9 +8,7 @@ $: << ROOT_PATH if File.directory?(ROOT_PATH) and not $:.include?(ROOT_PATH)
 require 'capistrano-exts/core_ext'
 
 # require Capistrano colors
-require 'capistrano/configuration'
 require 'capistrano/logger'
-require 'capistrano_colors/configuration'
 require 'capistrano_colors/logger'
 
 # Printed credentials
@@ -23,7 +21,7 @@ Capistrano::Logger.add_color_matcher({ :match => /WARNING:/, :color => :yellow, 
 Capistrano::Logger.add_color_matcher({ :match => /ERROR:/, :color => :red, :level => Capistrano::Logger::IMPORTANT, :prio => -20 })
 
 # Require requested receipts
-require 'capistrano-exts/receipts'
+require 'capistrano-exts/receipts' if defined?(Capistrano::Configuration)
 
 # Require all servers
 Dir["#{ROOT_PATH}/capistrano-exts/servers/*.rb"].each { |f| require f }