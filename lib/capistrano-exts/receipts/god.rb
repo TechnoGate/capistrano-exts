cb348999f39d589b612d9963fc4654ab7426a463
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-09-22 11:36:43 +0200

    MISC: Even if it's not common sence to start unicorn/god from the app role, this is needed for my setup (Check the TODO.)

--- lib/capistrano-exts/receipts/god.rb
+++ lib/capistrano-exts/receipts/god.rb
@@ -6,7 +6,7 @@ end
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :god do
     desc "start god, this starts up unicorn server"
-    task :start, :roles => :web, :except => {:no_release => true} do
+    task :start, :roles => :app, :except => {:no_release => true} do
       logs_path = fetch(:logs_path)
       god_config = fetch(:god_config, "#{fetch :current_path}/config/god.rb")
       god_binary = fetch(:god_binary, 'god')
@@ -14,17 +14,17 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "stop god, this shutdowns unicorn server"
-    task :stop, :roles => :web, :except => {:no_release => true} do
+    task :stop, :roles => :app, :except => {:no_release => true} do
       run "cd #{current_path} && #{god_binary} terminate"
     end
 
     desc "restart god, this restarts the unicorn server"
-    task :restart, :roles => :web, :except => {:no_release => true} do
+    task :restart, :roles => :app, :except => {:no_release => true} do
       run "cd #{current_path} && #{god_binary} restart"
     end
 
     desc "check if god is already running"
-    task :check_if_running, :roles => :web, :except => {:no_release => true} do
+    task :check_if_running, :roles => :app, :except => {:no_release => true} do
       'true' ==  capture("if #{god_binary} status; then echo 'true'; fi").strip
     end
   end
--- lib/capistrano-exts/receipts/unicorn.rb
+++ lib/capistrano-exts/receipts/unicorn.rb
@@ -6,7 +6,7 @@ end
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :unicorn do
     desc "[internal] Setup unicorn"
-    task :setup, :roles => :web, :except => {:no_release => true} do
+    task :setup, :roles => :app, :except => {:no_release => true} do
       unicorn_pid = fetch(:unicorn_pid, "#{fetch :shared_path}/pids/unicorn.pid")
 
       # Create the pids folder
@@ -16,7 +16,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "start unicorn"
-    task :start, :roles => :web, :except => {:no_release => true} do
+    task :start, :roles => :app, :except => {:no_release => true} do
       unicorn_binary = fetch(:unicorn_binary, 'unicorn_rails')
       unicorn_config = fetch(:unicorn_config, "#{fetch :current_path}/config/unicorn.rb")
       rails_env = fetch(:rails_env, 'production')
@@ -24,23 +24,23 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "stop unicorn"
-    task :stop, :roles => :web, :except => {:no_release => true} do
+    task :stop, :roles => :app, :except => {:no_release => true} do
       unicorn_pid = fetch(:unicorn_pid, )
       run "#{try_sudo} kill `cat #{unicorn_pid}`"
     end
 
     desc "unicorn reload"
-    task :reload, :roles => :web, :except => {:no_release => true} do
+    task :reload, :roles => :app, :except => {:no_release => true} do
       run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
     end
 
     desc "graceful stop unicorn"
-    task :graceful_stop, :roles => :web, :except => {:no_release => true} do
+    task :graceful_stop, :roles => :app, :except => {:no_release => true} do
       run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
     end
 
     desc "restart unicorn"
-    task :restart, :roles => :web, :except => {:no_release => true} do
+    task :restart, :roles => :app, :except => {:no_release => true} do
       stop
       start
     end
