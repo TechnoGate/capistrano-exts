4389acd878bb0179e6652ec56086bf45c43ccd78
Wael Nasreddine <wael.nasreddine@gmail.com>
2012-05-24 00:25:51 +0200

    Bump to version 1.14.1

--- lib/capistrano-exts/version.rb
+++ lib/capistrano-exts/version.rb
@@ -2,8 +2,8 @@ module Capistrano
   module Extensions
     module Version #:nodoc:
       MAJOR = 1
-      MINOR = 13
-      TINY = 2
+      MINOR = 14
+      TINY = 1
 
       ARRAY  = [MAJOR, MINOR, TINY]
       STRING = ARRAY.join(".")
